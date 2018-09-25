3159b220719dd2134492a443187258b101326580
Lars <larsmackenzie@gmail.com>
2018-09-25 13:47:29 -0500

    created table structure for all types of activities

--- src/components/ReportingTableComponent/ReportingTableComponent.js
+++ src/components/ReportingTableComponent/ReportingTableComponent.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 class ReportingTableComponent extends Component {
     render(){
         let content = null; 
-        if(this.props.activityData.activity_id === 1){
+        if(this.props.activityData.activity_id === 1 || this.props.activityData.activity_id === 3){
         content =(
                 <Table>
                 <TableHead>
@@ -19,6 +19,7 @@ class ReportingTableComponent extends Component {
                 </TableHead>
                 <TableBody>
                     {this.props.activityData.map((activity, i) => {
+                        return(
                         <TableRow key={i}>
                         <TableCell>{activity.pet_name}</TableCell>
                         <TableCell>{activity.type}</TableCell>
@@ -26,6 +27,7 @@ class ReportingTableComponent extends Component {
                         <TableCell>{activity.time}</TableCell>
                         <TableCell>{activity.owner_name}</TableCell>
                         </TableRow>
+                        );
                     })}
                 </TableBody>
               </Table>
@@ -37,33 +39,62 @@ class ReportingTableComponent extends Component {
                   <TableRow>
                     <TableCell>Pet Name</TableCell>
                     <TableCell>Activity</TableCell>
-        
+                    <TableCell>Date</TableCell>
+                    <TableCell>Time Start</TableCell>
+                    <TableCell>Time End</TableCell>
+                    <TableCell>Poop Check</TableCell>
+                    <TableCell>Notes</TableCell>
+                    <TableCell>Owner</TableCell>
                   </TableRow>
                 </TableHead>
+                <TableBody>
+                    {this.props.activityData.map((activity, i) => {
+                        return(
+                    <TableRow key={i}>
+                        <TableCell>{activity.pet_name}</TableCell>
+                        <TableCell>{activity.type}</TableCell>
+                        <TableCell>{moment(activity.date).format('MM-DD-YYYY')}</TableCell>
+                        <TableCell>{activity.time_start}</TableCell>
+                        <TableCell>{activity.time_end}</TableCell>
+                        <TableCell>{activity.poop_check}</TableCell>
+                        <TableCell>{activity.notes}</TableCell>
+                        <TableCell>{activity.owner_name}</TableCell>
+                    </TableRow>
+                        );
+                    })}
+                </TableBody>
               </Table>
             );
-    } else if (this.props.activity_id === 3){
-        content =(
-            <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Pet Name</TableCell>
-                <TableCell>Activity</TableCell>
-    
-              </TableRow>
-            </TableHead>
-          </Table>
-        );
-    }  else if (this.props.activity_id === 4){
+    } else if (this.props.activity_id === 4){
     content =(
         <Table>
         <TableHead>
           <TableRow>
             <TableCell>Pet Name</TableCell>
             <TableCell>Activity</TableCell>
-
+            <TableCell>Date</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Medication</TableCell>
+            <TableCell>Notes</TableCell>
+            <TableCell>Owner</TableCell>
           </TableRow>
         </TableHead>
+        <TableBody>
+        {this.props.activityData.map((activity, i) => {
+                        return(
+                    <TableRow key={i}>
+                        <TableCell>{activity.pet_name}</TableCell>
+                        <TableCell>{activity.type}</TableCell>
+                        <TableCell>{moment(activity.date).format('MM-DD-YYYY')}</TableCell>
+                        <TableCell>{activity.time_start}</TableCell>
+                        <TableCell>{activity.time_end}</TableCell>
+                        <TableCell>{activity.poop_check}</TableCell>
+                        <TableCell>{activity.notes}</TableCell>
+                        <TableCell>{activity.owner_name}</TableCell>
+                    </TableRow>
+                        );
+                    })}
+        </TableBody>
       </Table>
     );
 }
